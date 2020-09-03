0c447bf9e3e72207745268317ed255531f3ce50f
cycgattari <cycgattari@hotmail.com>
2020-09-03 19:30:44 -0300

    added view and template for detailed blog post well and css

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+# the post/,int pkk sepecifies a url patter
+# post means that teh url shoul begin with the word {Post}
+#
